c15325d9d073e229cc6ef998f7a52c482067be32
Elias Palavecino <eliaspalavecino125@gmail.com>
2023-09-17 20:19:46 -0300

    whatever

--- code-for-exploratory-data.R
+++ code-for-exploratory-data.R
@@ -1,2 +1,6 @@
 
 library(ggplot2)
+dataset <- read.csv(file="/Users/Usuario/Desktop/projectos visual studio/manufactury project/EconomiesOfScale.csv")
+#print(dataset)
+print(mean(dataset[["Manufacturing.Cost"]]))
+ggplot()
