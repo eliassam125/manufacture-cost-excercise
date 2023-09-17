24f0ae96ccba1f57c3ce1beb9cd1934d2bbf531f
Elias Palavecino <eliaspalavecino125@gmail.com>
2023-09-17 20:32:37 -0300

    functioning plot

--- code-for-exploratory-data.R
+++ code-for-exploratory-data.R
@@ -3,4 +3,8 @@ library(ggplot2)
 dataset <- read.csv(file="/Users/Usuario/Desktop/projectos visual studio/manufactury project/EconomiesOfScale.csv")
 #print(dataset)
 print(mean(dataset[["Manufacturing.Cost"]]))
-ggplot()
+print(ggplot(dataset, aes(x = Number.of.Units, y = Manufacturing.Cost)) +
+  stat_binhex() +
+  theme_bw() +
+  scale_fill_gradient(low = "white", high = "black") +
+  labs(x = "Number of Units", y = "Manufacturing Cost"))
